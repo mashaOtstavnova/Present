e52018ed5005da6a90cdd26adbfc3466ae565d62
mashaOtstavnova <machaotstavnova@mail.ru>
2016-12-15 12:42:37 +0500

    слияние

--- Presents/Presents/Presents.Core/ViewModels/NewPresentViewModel.cs
+++ Presents/Presents/Presents.Core/ViewModels/NewPresentViewModel.cs
@@ -2,49 +2,5 @@
 {
     public class NewPresentViewModel : BaseViewModel
     {
-        private string _name;
-        private string _description;
-        private decimal _price;
-        private byte[] _presentImage;
-
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                _name = value;
-                RaisePropertyChanged(() => Name);
-            }
-        }
-
-        public string Description
-        {
-            get { return _description; }
-            set
-            {
-                _description = value;
-                RaisePropertyChanged(() => Description);
-            }
-        }
-
-        public decimal Price
-        {
-            get { return _price; }
-            set
-            {
-                _price = value;
-                RaisePropertyChanged(() => Price);
-            }
-        }
-
-        public byte[] PresentImage
-        {
-            get { return _presentImage; }
-            set
-            {
-                _presentImage = value;
-                RaisePropertyChanged(() => PresentImage);
-            }
-        }
     }
 }
